10824a2d5ff26af2f39fd155033204f79ac63f3a
reaganmunene <reagannmunene@gmail.com>
2023-06-04 23:15:05 +0300

    fixed bugs again

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   const [bots, setBots] = useState([]);
+  const [army, setArmy] = useState([]);
+
 
  
   const handleAddBot = (bot) => {
@@ -15,10 +17,18 @@ function BotsPage() {
     setBots(bots.filter((bot) => bot.id !== botId));
   };
 
+  
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((resp) => resp.json())
+      .then((data) => setBots(bots));
+  }, );
+
+
   return (
     <div>
-      <YourBotArmy bots={bots} removeBot={handleRemoveBot} />
-      <BotCollection  bots={bots} addBot={handleAddBot} />
+      <YourBotArmy bots={bots} removeBot={handleRemoveBot} army={army} setArmy={setArmy} />
+      <BotCollection  bots={bots} addBot={handleAddBot} army={army} setArmy={setArmy}  />
     </div>
   );
 }
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,19 +1,17 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function YourBotArmy({ bots, removeBot }) {
+function YourBotArmy({ bots, removeBot, addBot, handleDeleteBot }) {
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
           <h2>Your Bot Army</h2>
-        </div>
-        <div className="row">
           {bots.map((bot) => (
-            <BotCard key={bot.id} bot={bot} removeBot={()=> removeBot} />
+            <BotCard key={bot.id} bot={bot} removeBot={()=> removeBot} deleteBot={handleDeleteBot} addBot={addBot} />
           ))}
         </div>
-      </div>
+        </div>
     </div>
   );
 }
